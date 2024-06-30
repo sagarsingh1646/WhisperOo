0a0653794408a5534b114f99e4ab8727c1229817
Sagar Singh <sagarsingh@Sagars-MacBook-Pro.local>
2024-06-30 22:04:18 +0530

    repush

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const usernameValidation = z
+    .string()
+    .min(4, "Username must be at least 4 characters")
+    .max(15, "Username must be no more than 15 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(8, { message: "Password must be at least 8 characters" }),
+});
